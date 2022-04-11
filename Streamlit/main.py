eb9b0a28d86bd991a692a974433e4d8468af9d41
Shawn Matyanga <87149956+starkjarvis21@users.noreply.github.com>
2022-04-11 21:53:51 +0200

    Machine Learning Model
    Model to predict objects

---
+++ Streamlit/main.py
@@ -0,0 +1,68 @@
+from helper import *
+import streamlit as st
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+import cv2
+
+sns.set_theme(style="darkgrid")
+
+sns.set()
+
+st.title('Object Detection ')
+
+def save_uploaded_file(uploaded_file):
+
+    try:
+
+        with open(os.path.join('static/videos',uploaded_file.name),'wb') as f:
+
+            f.write(uploaded_file.getbuffer())
+
+        return 1    
+
+    except:
+        return 0
+    
+search = st.text_input("Search here...",)
+uploaded_file = st.file_uploader("Upload the Video")
+
+
+if uploaded_file is not None:
+
+    if save_uploaded_file(uploaded_file): 
+        	
+        capture = cv2.VideoCapture(os.path.join('static/videos',uploaded_file.name))
+        frameNr = 0
+        
+        while (True):
+        
+            success, frame = capture.read()
+        
+            if success:
+                cv2.imwrite(f'static/images/{frameNr}.jpg', frame)
+                prediction = predictor(os.path.join(f'static/images/{frameNr}.jpg'))
+                prediction = decode_predictions(prediction)
+                predition_names = []
+                
+                for each in prediction[0]:
+                    predition_names.append(each[1])
+                    
+                if search in predition_names : 
+                    st.image(frame)
+                    st.text('Predictions :-')
+                    st.text(prediction)
+                    os.remove(os.path.join(f'static/images/{frameNr}.jpg'))
+            else:
+                break
+        
+            frameNr = frameNr+1
+        
+        capture.release()
+        os.remove('static/videos/'+uploaded_file.name)
+
+
+
+        
+
